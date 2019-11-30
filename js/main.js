1ba2327cca4dd7313fcb9c2d601bb6dfed28bca9
alexandr <childrenofbodom737@gmail.com>
2019-11-30 22:31:56 +0300

    Pricing Plan && Workers Section

--- js/main.js
+++ js/main.js
@@ -1,46 +1,39 @@
 $(document).ready(function() {
+    // ============= Portfolio Items ==========
+    $('button[filter]').click(function() {
+        if ($(this).attr("filter") == 'all') {
+            $('button[filter]').removeClass('active');
+            $(this).addClass('active');
+            if ($(this).attr("val") == 'off') {
 
-    $('button[filter="wd"]').click(function() {
-        if ($(this).attr("val") == 'off') {
+                $(this).addClass('active');
+                $('button[filter]').attr('val', 'off');
+                $(this).attr('val', 'on');
+    
+                $('.portfolio-images > div').hide(400);     
+                $('.portfolio-images > div[filter]').show(400); 
+            } 
+        } else {
+            $('button[filter]').removeClass('active');
+            $(this).addClass('active');
+            if ($(this).attr('val') == 'off') {
+                $('button[filter]').attr('val', 'off');
+                $(this).attr('val', 'on');
 
-            $('button[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-
-            $('.portfolio-images > div').hide(400);     
-            $('.portfolio-images > div[filter="wd"]').show(400); 
-        }  
+                $('.portfolio-images > div').hide(400);
+                var filter = $(this).attr("filter");
+                $('.portfolio-images > div[filter='+filter+']').show(400);
+            }
+        }   
     });
 
-    $('button[filter="ud"]').click(function() {
-        if ($(this).attr("val") == 'off') {
-            
-            $('button[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-
-            $('.portfolio-images > div').hide(400);     
-            $('.portfolio-images > div[filter="ud"]').show(400); 
-        }  
-    });
-
-    $('button[filter="moc"]').click(function() {
-        if ($(this).attr("val") == 'off') {
-            
-            $('button[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-
-            $('.portfolio-images > div').hide(400);     
-            $('.portfolio-images > div[filter="moc"]').show(400); 
-        }  
-    });
-
-    $('button[filter="all"]').click(function() {
-        if ($(this).attr("val") == 'off') {
-
-            $('button[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-
-            $('.portfolio-images > div').hide(400);     
-            $('.portfolio-images > div[filter]').show(400); 
-        }  
+    // ========= Slick Slider ==========
+    $('.workers').slick({
+        infinite:true,
+        slidesToShow:3,
+        slidesToScroll:3,
+        arrows:false,
+        dots:true,
+        dotsClass:'dots-style',
     });
 });
